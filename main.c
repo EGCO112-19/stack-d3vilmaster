22d5e567d6897d773a4b42a9e2a50e0cf2adf29d
Thanawat Tejapijaya <thanawat.tej@student.mahidol.edu>
2021-02-26 08:54:29 +0000

    finish มั้ง

--- main.c
+++ main.c
@@ -6,25 +6,37 @@
 int main(int argc, char **argv){
   printf("Checking the parentheses in argv arguments\n");
   int i,N,j;
+  char out;
   
   Stack s;
-
+  s.size = 0;
+  s.top = NULL;
 
  
   for(i=1;i<argc;i++){
    
      for(j=0;j<strlen(argv[i]);j++){
        /* Use stack to help with the parentheses*/
- 
- 
-
+      if(argv[i][j] == '{' || argv[i][j] == '['){
+         push(&s,argv[i][j]);
+         s.size +=1;
+         }
+      if(argv[i][j] == '}'){
+        out = pop(&s);
+        s.size -= 1;
+        if(out != '{')break;
+        }
+      if(argv[i][j] == ']'){
+        out = pop(&s);
+        s.size -= 1;
+        if(out != '[')break;
+        }
+      }
+     if(s.size>0){
+       pop_all(&s);
+       printf("The parentheses does not match successfully in %s\n", argv[i]);
      }
-
-
+     else{printf("The parentheses match successfull in %s\n",argv[i]);}
   }
-
-
-
-
    return 0;
 }
--- stack.h
+++ stack.h
@@ -8,7 +8,31 @@ typedef struct {
 }Stack;
 
 typedef Stack * StackPtr;
-void push(StackPtr s, char value);
-char pop(Stack s);
-void pop_all(Stack s);
+void push(StackPtr s, char value){
+  Node *new_node=(Node *)malloc(sizeof(Node));
+  if(new_node){
+  new_node-> data=value;
+  new_node-> nextPtr=s->top;
+  s->top=new_node;
+  }
+}
+char pop(StackPtr s){
+  NodePtr t =s->top;
+  char value;
+  value=t->data;
+  s->top=t->nextPtr;
+  free(t);
+  return value;
+}
+void pop_all(StackPtr s){
+  NodePtr t =s->top;
+  char value;
+  while(s->top != NULL){
+    value=t->data;
+    s->top=t->nextPtr;
+    printf("Popping %c\n",value);
+    free(t);
+  }
+}
+
 #endif
